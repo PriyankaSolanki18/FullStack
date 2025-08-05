e906a058208427896ee47c1a72f244fb48790af2
Priyanka Solanki <priyankasolanki.1820@gmail.com>
2025-08-05 19:44:07 +0530

    Initial Commit

---
+++ Backend/script.js
@@ -0,0 +1,16 @@
+// let n = 5;
+
+// for(let i=0; i<n; i++){
+//     console.log("hello, "+i);
+// }
+
+// console.log("Bye!!!");
+
+console.log(process.argv);
+
+let args = process.argv;
+
+// we are starting this loop from 2 as the first 2 arguments are default paths
+for(let i=2;i<args.length;i++){
+    
+}
