11e23958f8aa1e26dfbe6b6bd77e1128231878ed
Priyanka Solanki <priyankasolanki.1820@gmail.com>
2025-09-06 01:17:45 +0530

    Commit 06-09-2025

---
+++ Backend/math.js
@@ -0,0 +1,37 @@
+const sum = (a, b) => a+b;
+const mul = (a, b) => a*b;
+const g = 9.8;
+const PI = 3.14;
+
+module.exports = 123; // math.js module.exports ki help se 123 pre-set value export karegi scrit.js file ko filhal toh jab ham run karvaenge : node script.js -> o/p: 123
+
+
+/* 1.
+let obj={
+    sum: sum,
+    mul:mul,
+    g:g,
+    PI:PI
+};
+module.exports = obj;
+*/
+
+//2.par vale object ko ham ese bhi likh skte h
+module.exports = {
+    sum:sum,
+    mul:mul,
+    g:g,
+    PI:PI
+};
+
+//3.  
+// module.exports.sum = (a,b) => a+b;
+// module.exports.mul = (a,b) => a*b;
+// module.exports.g = 9.8;
+// module.exports.PI = 3.14;
+
+//4.
+// exports.sum = (a,b) => a+b;
+// exports.mul = (a,b) => a*b;
+// exports.g = 9.8;
+// exports.PI = 3.14;
--- Backend/script.js
+++ Backend/script.js
@@ -6,11 +6,26 @@
 
 // console.log("Bye!!!");
 
-console.log(process.argv);
+//console.log(process.argv);
 
-let args = process.argv;
-
+//let args = process.argv;
 // we are starting this loop from 2 as the first 2 arguments are default paths
-for(let i=2;i<args.length;i++){
-    
-}
+// for(let i=2;i<args.length;i++){
+//     console.log("Hello to ",args[i]);
+// }
+
+// script.js me math.js ke functions ko kese use ker skte h
+// module.exports : ke through ham ye ker sakte h
+const someValue = require("./math");  // require method ki help se script.js ne exported value ko import/use ker liya or variable me store kerva liya
+console.log(someValue);
+
+const math = require("./math");
+console.log(math);
+
+console.log(math.sum(2,2));
+console.log(math.PI);
+
+const info = require("./Fruits");
+console.log(info);
+console.log(info[0]);
+console.log(info[1].name);
